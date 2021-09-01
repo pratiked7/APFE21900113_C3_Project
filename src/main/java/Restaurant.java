0952991b6b92904186a0aa30a185935550fb62d7
pratiked7 <pratiked7@gmail.com>
2021-09-01 22:21:55 +0530

    Part 3: Solution

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -63,6 +63,11 @@ public class Restaurant {
     }
 
     public int getOrderValue(List<String> itemNames){
-        return 0;
+        int orderValue = 0;
+        for (String itemName : itemNames){
+            Item item = findItemByName(itemName);
+            orderValue += item.getPrice();
+        }
+        return orderValue;
     }
 }
